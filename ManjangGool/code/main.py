89395316c481ee94a33c44593a3a87f8ca99bf2c
davidKMJ <onedriveonly11@gmail.com>
2022-11-27 16:23:01 +0900

    ManjangGool, JoosangJuli #1

---
+++ ManjangGool/code/main.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from settings import * 
+from level import Level
+from support import *
+
+# Pygame setup
+pygame.init()
+screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+level = Level(level_map,screen)
+sky = import_image('../graphics/background/background.png', (screen_width, screen_height))
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	
+	screen.blit(sky, (0,0))
+	level.run(pygame.time.get_ticks())
+
+	pygame.display.update()
+	clock.tick(60)
---
+++ ManjangGool/code/settings.py
@@ -0,0 +1,15 @@
+level_map = [
+'                                                                ',
+'                                                                ',
+'                                   X                             ',
+'    P                              XXX                                    ',
+'   XXX                        XX   XXX                               ',
+'   XXX X    XX               XXX                                   ',
+'   XXX X  XXXX    XX  XX     XX                   ',
+'   XXXXX  XXXX    XX  XXX    X     XX           ',
+'   XXXXX XXXXXXX  XX  XXXX   XXX   XXX                 ',
+'   XXXXX XXXXXXX  XX  XXXX   XXX   XXXXX               ']
+
+tile_size = 64
+screen_width = 1040
+screen_height = len(level_map) * tile_size
